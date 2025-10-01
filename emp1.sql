f3dc97c7b42e7d789486568f72d0cb54a00eb3e6
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2025-10-01 11:26:16 +0530

    Update emp1.sql
    syntax errors solved

--- emp1.sql
+++ emp1.sql
@@ -1,5 +1,5 @@
-create table emp1(empno int(3), name char(25), basic char());
+create table emp1(empno int(3), name char(25), basic int(10));
 
-insert intp emp1(1,"raman",5000);
+insert into emp1(1,"raman",5000);
 
-select from emp1
+select * from emp1;
